554479b9ed22824d84ee79c228594f1d4f0d60c7
Lei Xu <eddyxu@gmail.com>
2022-05-12 09:55:37 -0700

    fix apt install

--- sql/rikai--0.1.sql
+++ sql/rikai--0.1.sql
@@ -23,14 +23,13 @@ CREATE FUNCTION ml.version()
 RETURNS TEXT
 AS $$
 	import rikai
-	return f"rikai: {rikai.__version__.version}"
+	return rikai.__version__.version
 $$ LANGUAGE plpython3u;
 
 -- Trigger to create a model inference function after
 -- creating a model entry.
 CREATE FUNCTION ml.create_model_trigger()
 RETURNS TRIGGER
-PARALLEL RESTRICTED
 AS $$
     model_name = TD["new"]["name"]
     plpy.info("Creating model: ", model_name)
@@ -60,9 +59,3 @@ CREATE TRIGGER create_model
 AFTER INSERT ON ml.models
 FOR EACH ROW
 EXECUTE FUNCTION ml.create_model_trigger();
-
-CREATE FUNCTION iou(box1 box, box2 box)
-RETURNS real
-PARALLEL SAFE
-AS $$
-$$ LANGUAGE SQL
