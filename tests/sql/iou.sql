b14874bc8a6b97bb956adad8021a945a8a3c92c7
Lei Xu <lei@eto.ai>
2022-05-16 12:31:15 -0700

    error case

--- tests/sql/iou.sql
+++ tests/sql/iou.sql
@@ -1,13 +1,19 @@
 -- Test IOU related tests.
 BEGIN;
 
-SELECT plan(1);
+SELECT plan(2);
 
 SELECT results_eq(
 	'select iou(box(point(1, 2), point(3,4)), box(point(1, 2), point(3, 4))) as iou;',
-	ARRAY[0],
+	ARRAY[1.0],
 	'IOU of two same boxes',
-	)
+)
+
+SELECT results_eq(
+	'select iou(box(point(1, 2), point(3, 4)), box(point(10, 15), point(12, 30)));',
+	ARRAY[0.0],
+	'Two boxes do not have intersection',
+)
 
 SELECT * FROM finish();
 ROLLBACK;
